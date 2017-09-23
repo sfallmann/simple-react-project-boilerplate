2de70da24dab04844d7bac989d0349e2bf946821
Sean Fallmann <jorundr@hotmail.com>
2017-09-22 23:37:03 -0400

    Add app.js in src and server folders

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* 
+
+  Import components here:
+
+  import ComponentName from './components/ComponentName';
+
+*/
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+/*
+
+  Render components here:
+
+  ReactDOM.render(<ComponentName />, document.getElementById('targetElement'))
+
+*/
